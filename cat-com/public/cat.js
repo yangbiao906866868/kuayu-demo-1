b0cddce1a74546a112d424ab03ba01f57eaea667
yangbiao <906866868@qq.commkdir>
2021-01-30 07:14:47 +0800

    跨域

---
+++ cat-com/public/cat.js
@@ -0,0 +1,32 @@
+const request = new XMLHttpRequest()
+request.open('GET','http://qq.com:8888/cat.json')
+request.onreadystatechange = () =>{
+    if(request.readyState===4 &&request.status===200){
+        console.log(request.response)
+    }
+}
+request.send()
+
+
+function jsonp(url){
+    return new Promise((resolve,reject)=>{
+        const random ='frankJSONPCallbackName'+Math.random()
+        window[random] = (data)=>{
+            resolve(data)
+        }
+        const script = document.createElement('script')
+        script.src = `${url}?callback=${random}`
+        script.onload = () =>{
+            script.remove()
+    }
+        script.onerror = () =>{
+            reject()
+        }
+        document.body.appendChild(script)
+    })
+}
+
+jsonp('http://qq.com:8888/cat.js')
+    .then((data) =>{
+        console.log(data)
+    })
